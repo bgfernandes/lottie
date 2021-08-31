82aafb24ce59d03f91d22691a3032c6b670bf2bb
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-08-31 10:20:34 +0800

    Adds dummy use of apollo-client SSR and CSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,49 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
+import { gql, useQuery } from '@apollo/client'
+import client from '../apollo/client'
 
-const Home: NextPage = () => {
+const HELLO_MESSAGE_QUERY = gql`
+  query HelloMessage {
+    hello {
+      message
+    }
+  }
+`
+
+export const getServerSideProps: GetServerSideProps = async (_context) => {
+  const { data } = await client.query({
+    query: HELLO_MESSAGE_QUERY,
+  })
+
+  return {
+    props: { serverSideData: data },
+  }
+}
+
+type HomeProps = {
+  serverSideData: {
+    hello: {
+      message: String
+    }
+  }
+}
+
+const Home: NextPage<HomeProps> = ({ serverSideData }) => {
+  const { data } = useQuery(HELLO_MESSAGE_QUERY)
   return (
     <div>
       Hello world.
 
       <p className="flex justify-center">
         This page is using tailwindcss.
+      </p>
+
+      <p>
+        This message was fetched from the graphql API on the server side: { serverSideData.hello.message }
+      </p>
+
+      <p>
+        This message was fetched from the graphql API on the client side: { data ? data.hello.message : 'loading' }
       </p>
     </div>
   )
